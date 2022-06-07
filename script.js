d3866177b215ef121e14a7fcdb6f10ca2c5adb66
DespinaChrist <despinachristod@gmail.com>
2022-06-07 21:34:50 +0300

    script added

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-const number1 = document.getElementById("n1");
-const number2 = document.getElementById("n2");
-const number3 = document.getElementById("n3");
-const number4 = document.getElementById("n4");
-const number5 = document.getElementById("n5");
-const number6 = document.getElementById("n6");
-const bonusNumber = document.getElementById("bn");
-const btn = document.getElementById("genButton");
+'use strict';
 
-function RandomNumberGen(number){
+const number1 = document.getElementById('n1');
+const number2 = document.getElementById('n2');
+const number3 = document.getElementById('n3');
+const number4 = document.getElementById('n4');
+const number5 = document.getElementById('n5');
+const number6 = document.getElementById('n6');
+const bonusNumber = document.getElementById('bn');
+const btn = document.getElementById('genButton');
+
+function RandomNumberGen(){
     let getRandom = Math.floor( (Math.random() * (49 - 1) ) + 1 );
-    retunr = getRandom;
+    return getRandom;
 }
 
-function handler(){
-    number1.textContent = RandomNumberGen(number1);
-    number2.textContent = RandomNumberGen(number2);
-    number3.textContent = RandomNumberGen(number3);
-    number4.textContent = RandomNumberGen(number4);
-    number5.textContent = RandomNumberGen(number5);
-    number6.textContent = RandomNumberGen(number6);
-    bonusNumber.textContent = RandomNumberGen(bonusNumber);
-}
+function method() {
+    number1.textContent = RandomNumberGen();
+    number2.textContent = RandomNumberGen();
+    number3.textContent = RandomNumberGen();
+    number4.textContent = RandomNumberGen();
+    number5.textContent = RandomNumberGen();
+    number6.textContent = RandomNumberGen();
+    bonusNumber.textContent = RandomNumberGen();
+};
 
-btn.addEventListener("click", handler);
+btn.addEventListener('click', method);
+
